186c5b58c96b22433dd1b5313cc75b9d7df3777d
Manish <100868488+kolamanish@users.noreply.github.com>
2025-05-02 12:13:13 -0400

    Update to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+x = st.slider("Select a value")
+st.write(x, "squared is", x * x)
